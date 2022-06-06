f8600970a36ebb3d648d2016630e80187a19f21d
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-06-06 18:18:21 -0400

    Update FBPoster-Pi.py
    fixing various syntax things and cleaning up the code a little

--- FBPoster-Pi.py
+++ FBPoster-Pi.py
@@ -6,7 +6,7 @@ from datetime import datetime # used for date and time in the FB log posting so 
 from googleapiclient.discovery import build # for spreadsheet stuff
 from google.oauth2 import service_account # also for spreadsheet stuff
 import facebook # to add captions and comments to existing posts.
-import json
+import json # to decipher the dictionary we get back in return from FB servers. (Mainly this is used to edit captions to the posts). 
 
 page_id = "100516779334651" # ID of the FB page, can be found on FB > page > about > Page ID
 
@@ -25,52 +25,56 @@ service = build('sheets', 'v4', credentials=creds) # builds a package with all t
 # Call the Sheets API
 sheet = service.spreadsheets()
 
-result = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
+result_fb = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
                             range="FB-Poster-Log!A:H").execute() # to specify this variable as all of the FB log sheet values
 
-values = result.get('values', []) #get values from spreadsheet
+values_fb = result_fb.get('values', []) #get values from spreadsheet
 
-result2 = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
+result_rg = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
                             range="Reddit-Grabber-Log!A:F").execute() # to specify this variable as all of the reddit grabber spreadsheet
 
-values2 = result2.get('values', []) #get values from spreadsheet
+values_rg = result_rg.get('values', []) #get values from spreadsheet
 
 count = 0 # establishes count
 
 #setup primary for loop
-for string in values:
+for string in values_fb:
     #create an empty list to fill with data
-    random1 = []
+    chosen_post = []
 
     #pick a random value from reddit spreadsheet
-    random1 = random.choice(values2)
+    chosen_post = random.choice(values_rg)
 
     # basically labeling each item in the index if we ever need to use them
-    random_title = random1[0] #grab element one from random list item, this is the title of the randomly grabbed post
-    random_id = random1[1] #grab element two from random list item, this is the id of the randomly grabbed post
-    random_permalink = random1[2] #grab element three from random list item, this is permalink of the randomly grabbed post
-    random_url = random1[3] #grab element four from random list item, this is the url to the image itself, of the randomly grabbed post
-    random_size = random1[4] #grab element five from random list item, this is file size of the randomly grabbed post
-    random_hash = random1[5] #grab element six from random list item, this is the image hash of the reddit image
+    random_title = chosen_post[0] #grab element one from random list item, this is the title of the randomly grabbed post
+    random_id = chosen_post[1] #grab element two from random list item, this is the id of the randomly grabbed post
+    random_permalink = chosen_post[2] #grab element three from random list item, this is permalink of the randomly grabbed post
+    random_url = chosen_post[3] #grab element four from random list item, this is the url to the image itself, of the randomly grabbed post
+    random_size = chosen_post[4] #grab element five from random list item, this is file size of the randomly grabbed post
+    random_hash = chosen_post[5] #grab element six from random list item, this is the image hash of the reddit image
 
     random_hash_string = str(random_hash) # converts hash to string value just to be safe for the next step
 
     #create a flatten list from the list of lists, this way we can do searches thorugh it lke hash check for example
-    flatlist_fb_sheet = [item for items in values for item in items]
+    flatlist_fb_sheet = [item for items in values_fb for item in items]
 
     # look for hash in the FB post log
     check_hash = random_hash_string in flatlist_fb_sheet
 
-    # if random_hash_string not in values: # scan all ouf our FB logs to see if the hash of the image we picked has already been posted.
     if check_hash is False:
+        
         #make sure that the hash did not come up in the search results - if none then it's not a duplicate image
         msg = str(random_url) #the message we are sending to the fb server and make sure it's a string
+        
         post_url = 'https://graph.facebook.com/{}/photos'.format(page_id) #url that we will send our HTTP request to - to post it to FB
+        
         payload = {
             "url": msg, #injecting our str(url) into the "url" section of the link itself
             "access_token": config.config_stuff['FB_Access_Token'] # giving our api token to our page referenced in the secret config file
           }
+        
         r = requests.post(post_url, data=payload) #wraps up all the above info into a clean variable we can work with
+        
         print(r.text) # print the return text from FB servers to make sure the message went through properly or if not look at errors
 
         r_text_str = str(r.text)  # defining this as a string variable to use later (just to be safe -- probably redundant).
@@ -80,8 +84,8 @@ for string in values:
         #make sure the post we sent is not an error and actually sent through as a real post
         check_no_error = page_id in r_text_str
 
-        # True = no error
-        if check_no_error is True:  # When you send a post to FB, if the post goes through it will return the page ID in the r.text, so this checks to make sure we actually made a real post instead of sending a bunch of error'd posts that didn't actually create a real post.
+        # If "check_no_error" is True then that means there were no error return code from FB servers, meaning the post went through
+        if check_no_error == True:  # When you send a post to FB, if the post goes through it will return the page ID in the r.text, so this checks to make sure we actually made a real post instead of sending a bunch of error'd posts that didn't actually create a real post.
             random_permalink_string = str(random_permalink)  # without concatenating, random_permalink only gives back /r/.... without the url part of it
 
             #create an empty list to store our list of values to write to teh spreadsheet (spreadsheet requires a 2d list aka list of lists
