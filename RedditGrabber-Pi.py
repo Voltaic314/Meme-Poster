d961b70c625dd69bfe2e368827f7baef99dd9a14
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-06-08 14:20:31 -0400

    Fixed the formatting so it can collect 4 posts now
    The bot will now pick a random subreddit, collect 4 posts that meet the criteria, then stop. In this instance it is pulling more from each source when it picks a source. This does result in less variation overall (in the instance for example it picks a subreddit A, grabs 4 posts from A, then the next time it runs it ends up picking A again and grab 4 more posts, that's 8 posts grabbed in 4 hours from a single source). But overall randomness will eventually level this out. If you do the math long term it doesn't have any major long term repercussions doing it this way. Mainly the reason I wanted to do this is so that we don't have to run the code so frequently. Now it only has to run every 2 hours instead of every 30 minutes. Which lessens the chances of failure and helps it build up a queue if for some reason something goes down.

--- RedditGrabber-Pi.py
+++ RedditGrabber-Pi.py
@@ -2,12 +2,13 @@ import config # used to get the secret sensitive info needed for our APIs - not 
 import praw # used for the reddit api - this is 100% needed for this code to work
 import requests # needed to get image file size before we download images (to make sure we don't download images too large that we can't upload elsewhere).
 import os # needed to get the file paths
-import random # needed to pick a random subreddit to grab data from. In theory you don't have to pick a random one, you could do all at once or just one, either or.
+import random #needed to pick a random subreddit to grab data from. In theory you don't have to pick a random one, you could do all at once or just one, either or.
 from googleapiclient.discovery import build # python.exe -m pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 from google.oauth2 import service_account # this and the above package are for the spreadsheet usage -- the pip command is a pain so I pasted it above.
 from PIL import Image # for image hashing
 import imagehash # also for image hashing
-import pytesseract # used for optical character recognition within images, basically pulling text out of images so we can analyze it
+import re # used to split strings into a list of words later down the program
+import pytesseract # used for image recognition
 import cv2 # used for parsing data and converting images before putting into tesseract OCR
 
 #establishing our creds to gain access to the API before we start doing any calls.
@@ -30,43 +31,39 @@ service = build('sheets', 'v4', credentials=creds) # builds a package with all t
 # Call the Sheets API
 sheet = service.spreadsheets()
 
-result_fb = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
+result = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
                             range="FB-Poster-Log!A:H").execute()
 
-values_fb = result_fb.get('values', []) #get values from spreadsheet
+values = result.get('values', []) #get values from spreadsheet
 
-result_rg = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
+result2 = sheet.values().get(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
                             range="Reddit-Grabber-Log!A:F").execute() # to specify this variable as all of the reddit grabber spreadsheet
 
-values_rg = result_rg.get('values', []) #get values from spreadsheet
+values2 = result2.get('values', []) #get values from spreadsheet
 
 #list of subreddits to grab memes from
 subreddit_list = ["memes", "dankmemes", "shitposting", "Unexpected", "Wholesomememes", "me_irl", "meme",
                   "Memes_Of_The_Dank", "starterpacks", "animemes", "funny"]
 
-# list of bad words / topics to avoid in our posts
-# mainly what I'm trying to avoid here are images with really bad / horny type words in them. overly sexual memes, political memes,
-# memes mocking religion, any post about horrifying diseases, dark topics, bigotry memes, incel memes, and also meta level memes about reddit,
-# primarily the latter because even though it's getting memes from reddit, I want it to feel like the memes are universal and could be posted anywhere.
-
+#list of bad words / topics to avoid in our posts
 bad_topics = ["faggot", "femboy", "nigger", "fat", "skinny", "horny", "masturbate", "anal", "sex",
               "racist", "homophobic", "rape", "rapist", "BDSM", "dom", "fucked", "hentai",
               "Joe Biden", "Biden", "Trump", "Donald Trump", "disease", "symptom", "Parkinson", "Alzhemier", "memeory loss",
               "COVID", "covid-19", "Virus", "bacteria", "Pandemic", "quarantine", "NATO", "Ukraine", "Russia", "Putin", "fatal",
               "lethal", "no cure", "cock", "pussy", "dick", "vagina", "penis", "reddit",
-              "u/", "/r/", "reddit", "feminist", "qanon", "shooting", "Uvalde",]
+              "u/", "/r/", "feminists", "qanon", "shooting", "Uvalde",]
 
 #picks a random subreddit from the above list
-subreddit = reddit.subreddit(random.choice(subreddit_list)).hot(limit=None)
+subreddit = reddit.subreddit(random.choice(subreddit_list)).top(time_filter="day", limit=None)
 
-
-## Flattening the lists to help search for strings in those lists. All we need to do is check if they are there and get their indexes.
 
 #flatten the list of lists returned from the fb poster spreadsheet
-flatlist_fb = [item for items in values_fb for item in items]
+flatlist_fb = [item for items in values for item in items]
 
 #flatten the list of lists returned from the reddit grabber spreadsheet
-flatlist_rg =[item for items in values_rg for item in items]
+flatlist_rg =[item for items in values2 for item in items]
+
+#reader = easyocr.Reader(['en'], gpu=False)
 
 #initializes loop
 count = 0
@@ -78,30 +75,30 @@ for submission in subreddit:
 
         if submission.id not in flatlist_rg: #make sure we don't grab the same reddit post twice -- dont need to hash images that we've already done before
 
-            if submission.spoiler == False: # make sure the post is not flagged as a spoiler
+            if submission.spoiler is False: # make sure the post is not flagged as a spoiler
 
                 if not any(x in submission.title for x in bad_topics): # make sure no bad words are in the submission post titles.
 
                     if submission.over_18 == False: # make sure the post is not flagged as NSFW -- side note: this sucks, people still post bad crap anyways and mods don't remove it. be careful which subreddits you are scanning even with this on.
-                        r = requests.get(url) # defines R variable as grabbing data from our selected url
+                        r = requests.get(url) #defines R variable as grabbing data from our selected url
                         length = float(r.headers.get('content-length')) / 1000 # divides file size by 1000 so we can get how many kilobytes it is
 
-                        if float(length) < 4000: # if it is less than 4 MB or 4000 KB (alternatively for cleaner numbers you can divide by 1,000,000 and do < 4 but eh
-                            open("image.jpg", 'wb').write(r.content) # download the image from the "url" variable link using requests function
-                            hash = imagehash.dhash(Image.open("image.jpg")) # hash the image we just saved
+                        if float(length) < 4000: # if it is less than 4 MB or 4000 KB (alternatively for cleaner numbers you can divide by 1,000,000 and do < 4 but e
+                            open("image.jpg", 'wb').write(r.content) #download the image from the "url" variable link using requests function
+                            hash = imagehash.dhash(Image.open("image.jpg")) #hash the image we just saved
 
                             # define all of our variables as strings to be used later
-                            sub_title_string = str(submission.title)
-                            sub_id_string = str(submission.id)
-                            sub_permalink_string = (str("https://www.reddit.com") + str(submission.permalink))
-                            sub_length_string = str(length)
-                            sub_hash_string = str(hash)
+                            submission_title_string = str(submission.title)
+                            submission_id_string = str(submission.id)
+                            submission_permalink_string = (str("https://www.reddit.com") + str(submission.permalink))
+                            submission_length_string = str(length)
+                            submission_hash_string = str(hash)
 
                             #check to make sure the hash of the image we just tested is in the spreadsheet. False means that it's not which means it's not a duplicate image (which is good).
-                            check_hash_fb = sub_hash_string in flatlist_fb
+                            check_hash_fb = submission_hash_string in flatlist_fb
 
                             #check to make sure the hash of the image we just tested is in the reddit grabber spreadsheet. (We want false values only here).
-                            check_hash_rg = sub_hash_string in flatlist_rg
+                            check_hash_rg = submission_hash_string in flatlist_rg
 
 
                             if check_hash_fb == False: #make sure the hash is not in the reddit post list already
@@ -130,8 +127,8 @@ for submission in subreddit:
 
                                     # this section cleans up the list to remove the "\n' from each string in the newly created list
                                     replaced_list = []
-                                    for strings in list_text:
-                                        replaced_list.append(strings.replace("\n", ""))
+                                    for s in list_text:
+                                        replaced_list.append(s.replace("\n", ""))
 
                                     #check to see if within the meme itself if there are bad words in the list above
                                     check_ocr_bad_topics = [word for word in replaced_list if word in bad_topics]
@@ -143,20 +140,30 @@ for submission in subreddit:
                                         Spreadsheet_Values_Append = []
 
                                         #append list with data from variables above -- I wanted to do this with the count being 4 and have a list of lists containing 4 list which each represent rows but couldn't figure out the formatting so this will do for now.
-                                        Spreadsheet_Values_Append.append([sub_title_string, sub_id_string, sub_permalink_string, url,sub_length_string, sub_hash_string])
-                                        ## TODO: Fix formatting of this nonsense so we can post more than link in a single script run
+                                        Spreadsheet_Values_Append.append([submission_title_string, submission_id_string, submission_permalink_string, url,submission_length_string, submission_hash_string])
+
+                                        print(Spreadsheet_Values_Append)
+
+                                        request = sheet.values().append(
+                                            spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
+                                            range="Reddit-Grabber-Log!A:F", valueInputOption="USER_ENTERED",
+                                            body={"values": Spreadsheet_Values_Append}).execute()
+
+                                        print("Post logged to Reddit Grabber Spreadsheet")
 
                                         #increases the count to break the loop
-                                        count += 1
+                                        count = count + 1
 
-                                        for x in subreddit:  # starting for loop to establish checking the count
-                                            if count == 1:  # this value determines how many images to post to the reddit spreadsheet
-                                                break  # ... break the loop. Note that the count will only go up if a successful post goes through. Meaning it can keep constantly posting errors over and over until it finally gets a successful post.
-                                        break # break out of the original for loop above
+                                        #if the count reaches 4 (meaning it has collected 4 posts) from the randomly chosen subreddit, then we're good, if not keep going until we get 4.
+                                        if count == 4:
+                                            break # break means to break out of the loop, meaning we're good
+                                        else:
+                                            continue # continue in this instance means to start the loop over (meaning go get another post that meet the criteria
 
-# write 2d list to the final row of the spreadsheet (this is where it adds what it just found to the spreadsheet finally).
-append_rg = sheet.values().append(spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
-                                range="Reddit-Grabber-Log!A:F", valueInputOption="USER_ENTERED",
-                                body={"values": Spreadsheet_Values_Append}).execute()
+                                    # if this part isn't included, it will run through 4 iterations, but may not end up grabbing any posts out of the 4 attempts that meet the criteria.
+                                    else:
+                                        continue # continue in this case meaning, if the post we grabbed doesn't meet the criteria, keep trying until we get one, then start increasing the count.
 
-print("Post logged to Reddit Grabber Spreadsheet")
+
+# just for my own sanity, to make sure we completed the whole loop and script. THe proverbial "The end." lol
+print("\nAll posts have been logged to the spreadsheet accordingly.")
