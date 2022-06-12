79d3c68eb35d858a0f326cef89d75c51b3100f6d
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-06-11 20:03:40 -0400

    Cleaned up a few nested IF statements

--- RedditGrabber-Pi.py
+++ RedditGrabber-Pi.py
@@ -82,123 +82,115 @@ for submission in subreddit:
 
     # Makes sure that the url we got from the api is a string variable
     url = str(submission.url)
+            
+    # 1. make sure the post is an image
+    # 2. make sure we don't grab the same reddit post twice -- dont need to hash images that we've already done before
+    # 3. make sure the post is not flagged as a spoiler or NSFW
+    # 4. make sure no bad words are in the submission post titles.
+    if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png") and if submission.id not in flatlist_rg and if not submission.spoiler and not submission.over_18 and if not any(x in submission.title for x in bad_topics):
 
-    # Make sure the post is an image
-    if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
+        # defines R variable as grabbing data from our selected url
+        r = requests.get(url)
 
-        # make sure we don't grab the same reddit post twice -- dont need to hash images that we've already done before
-        if submission.id not in flatlist_rg:
+        # divides file size by 1000 so we can get how many kilobytes it is
+        length = float(r.headers.get('content-length')) / 1000
 
-            # make sure the post is not flagged as a spoiler
-            if submission.spoiler is False:
+        # if it is less than 4 MB or 4000 KB (alternatively for cleaner numbers you can divide by 1,000,000 and do < 4 but e
+        if float(length) < 4000:
 
-                # make sure no bad words are in the submission post titles.
-                if not any(x in submission.title for x in bad_topics):
+            # download the image from the "url" variable link using requests function
+            open("image.jpg", 'wb').write(r.content)
 
-                    # make sure the post is not flagged as NSFW -- side note: this sucks, people still post bad crap anyways and mods don't remove it. be careful which subreddits you are scanning even with this on.
-                    if submission.over_18 == False:
+            # hash the image we just saved
+            hash = imagehash.dhash(Image.open("image.jpg"))
 
-                        # defines R variable as grabbing data from our selected url
-                        r = requests.get(url)
+            # define all of our variables as strings to be used later
+            submission_title_string = str(submission.title)
+            submission_id_string = str(submission.id)
+            submission_permalink_string = (str("https://www.reddit.com") + str(submission.permalink))
+            submission_length_string = str(length)
+            submission_hash_string = str(hash)
 
-                        # divides file size by 1000 so we can get how many kilobytes it is
-                        length = float(r.headers.get('content-length')) / 1000
+            #check to make sure the hash of the image we just tested is in the spreadsheet. False means that it's not which means it's not a duplicate image (which is good).
+            check_hash_fb = submission_hash_string in flatlist_fb
 
-                        # if it is less than 4 MB or 4000 KB (alternatively for cleaner numbers you can divide by 1,000,000 and do < 4 but e
-                        if float(length) < 4000:
+            #check to make sure the hash of the image we just tested is in the reddit grabber spreadsheet. (We want false values only here).
+            check_hash_rg = submission_hash_string in flatlist_rg
 
-                            # download the image from the "url" variable link using requests function
-                            open("image.jpg", 'wb').write(r.content)
+            # make sure the hash is not in the FB post sheet or the RG sheet already
+            if not check_hash_fb and if not check_hash_rg:
 
-                            # hash the image we just saved
-                            hash = imagehash.dhash(Image.open("image.jpg"))
+                ##run OCR
+                #point to where the tesseract files are in our directory
+                pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
-                            # define all of our variables as strings to be used later
-                            submission_title_string = str(submission.title)
-                            submission_id_string = str(submission.id)
-                            submission_permalink_string = (str("https://www.reddit.com") + str(submission.permalink))
-                            submission_length_string = str(length)
-                            submission_hash_string = str(hash)
+                #read BGR values from image
+                img=cv2.imread('image.jpg')
 
-                            #check to make sure the hash of the image we just tested is in the spreadsheet. False means that it's not which means it's not a duplicate image (which is good).
-                            check_hash_fb = submission_hash_string in flatlist_fb
+                #convert BGR values to RGB values
+                img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-                            #check to make sure the hash of the image we just tested is in the reddit grabber spreadsheet. (We want false values only here).
-                            check_hash_rg = submission_hash_string in flatlist_rg
+                #give us the resulting text (strings) from the image
+                ocr_result = pytesseract.image_to_string(img)
 
-                            # make sure the hash is not in the FBpost list already
-                            if check_hash_fb == False:
+                # remove the image we just saved (since we don't actually need the file after hashing it)
+                os.remove("image.jpg")  
 
-                                # make sure the hash is not in the reddit post list already
-                                if check_hash_rg == False:
+                #this function converts the text from OCR into a list of individual strings, where each string is an element in a list
+                def Convert(string):
+                    li = list(string.split(" "))
+                    return li
+                list_text = Convert(ocr_result)
 
-                                    ##run OCR
-                                    #point to where the tesseract files are in our directory
-                                    pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+                # this section cleans up the list to remove the "\n' from each string in the newly created list
+                replaced_list = []
+                for strings in list_text:
+                    replaced_list.append(strings.replace("\n", ""))
 
-                                    #read BGR values from image
-                                    img=cv2.imread('image.jpg')
+                #check to see if within the meme itself if there are bad words in the list above
+                check_ocr_bad_topics = [word for word in replaced_list if word in bad_topics]
 
-                                    #convert BGR values to RGB values
-                                    img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                # if no matches of bad topics in the ocr text, then proceed. But if so, try a new image.
+                if not check_ocr_bad_topics:
 
-                                    #give us the resulting text (strings) from the image
-                                    ocr_result = pytesseract.image_to_string(img)
+                    # create an empty list to store data
+                    Spreadsheet_Values_Append = []
 
-                                    # remove the image we just saved (since we don't actually need the file after hashing it)
-                                    os.remove("image.jpg")  
+                    #append list with data from variables above -- I wanted to do this with the count being 4 and have a list of lists containing 4 list which each represent rows but couldn't figure out the formatting so this will do for now.
+                    Spreadsheet_Values_Append.append([submission_title_string, submission_id_string, submission_permalink_string, 
+                                                      url, submission_length_string, submission_hash_string])
 
-                                    #this function converts the text from OCR into a list of individual strings, where each string is an element in a list
-                                    def Convert(string):
-                                        li = list(string.split(" "))
-                                        return li
-                                    list_text = Convert(ocr_result)
+                    # print what is going to be uploaded to the spreadsheet, primarily just use this for testing purposes but leave it on anyways
+                    print(Spreadsheet_Values_Append)
 
-                                    # this section cleans up the list to remove the "\n' from each string in the newly created list
-                                    replaced_list = []
-                                    for s in list_text:
-                                        replaced_list.append(s.replace("\n", ""))
+                    # make a request to sheets API with the 2d list we just made to append to the bottom of the spreadsheet as a new row. 
+                    request = sheet.values().append(
+                        spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
+                        range="Reddit-Grabber-Log!A:F", valueInputOption="USER_ENTERED",
+                        body={"values": Spreadsheet_Values_Append}).execute()
 
-                                    #check to see if within the meme itself if there are bad words in the list above
-                                    check_ocr_bad_topics = [word for word in replaced_list if word in bad_topics]
+                    # again mostly used for testing and troubleshooting but leave it on anyways for the sake of human sanity
+                    print("Post logged to Reddit Grabber Spreadsheet")
 
-                                    # if no matches of bad topics in the ocr text, then proceed. But if so, try a new image.
-                                    if not check_ocr_bad_topics:
+                    #increases the count to break the loop
+                    count += 1
 
-                                        # create an empty list to store data
-                                        Spreadsheet_Values_Append = []
+                    #if the count reaches 4 (meaning it has collected 4 posts) from the randomly chosen subreddit, then we're good, if not keep going until we get 4.
+                    if count == 4:
 
-                                        #append list with data from variables above -- I wanted to do this with the count being 4 and have a list of lists containing 4 list which each represent rows but couldn't figure out the formatting so this will do for now.
-                                        Spreadsheet_Values_Append.append([submission_title_string, submission_id_string, submission_permalink_string, url,submission_length_string, submission_hash_string])
+                        # break means to break out of the loop, meaning we're good
+                        break 
 
-                                        print(Spreadsheet_Values_Append)
+                    else:
 
-                                        request = sheet.values().append(
-                                            spreadsheetId=config.config_stuff4['SAMPLE_SPREADSHEET_ID'],
-                                            range="Reddit-Grabber-Log!A:F", valueInputOption="USER_ENTERED",
-                                            body={"values": Spreadsheet_Values_Append}).execute()
+                        # continue in this instance means to start the loop over (meaning go get another post that meet the criteria
+                        continue 
 
-                                        print("Post logged to Reddit Grabber Spreadsheet")
+                # if this part isn't included, it will run through 4 iterations, but may not end up grabbing any posts out of the 4 attempts that meet the criteria.
+                else:
 
-                                        #increases the count to break the loop
-                                        count = count + 1
-
-                                        #if the count reaches 4 (meaning it has collected 4 posts) from the randomly chosen subreddit, then we're good, if not keep going until we get 4.
-                                        if count == 4:
-
-                                            # break means to break out of the loop, meaning we're good
-                                            break 
-                                        
-                                        else:
-
-                                            # continue in this instance means to start the loop over (meaning go get another post that meet the criteria
-                                            continue 
-
-                                    # if this part isn't included, it will run through 4 iterations, but may not end up grabbing any posts out of the 4 attempts that meet the criteria.
-                                    else:
-
-                                        # continue in this case meaning, if the post we grabbed doesn't meet the criteria, keep trying until we get one, then start increasing the count.
-                                        continue 
+                    # continue in this case meaning, if the post we grabbed doesn't meet the criteria, keep trying until we get one, then start increasing the count.
+                    continue 
 
 
 # just for my own sanity, to make sure we completed the whole loop and script. THe proverbial "The end." lol
